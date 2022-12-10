7bfe70b562f5c0d3efc79bd97ca5f0daf91b2c9c
Narender548 <narenderreddy548@gmail.com>
2022-12-10 12:31:54 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # Load the libraries
 from fastapi import FastAPI, HTTPException
 from joblib import load
+import pickle
 
 # Load the model
 model = pickle.load(open('LRM.sav', 'rb'))
 
-
 # Initialize an instance of FastAPI
 app = FastAPI()
 
-# Class which describes a single flower measurements
+# Class which describes input variables
 class IrisSpecies(BaseModel):
     host_is_superhost : int
     accommodates : int
@@ -18,9 +18,6 @@ class IrisSpecies(BaseModel):
     bathrooms : int
     number_of_reviews : int
 
-# Make a prediction based on the user-entered data
-# Returns the predicted price with its respective probability
-
 # Define the default route 
 @app.get("/")
 def root():
@@ -28,6 +25,9 @@ def root():
 
 # Define the route to the price predictor
 @app.post('/predict_dynamic_price')
+
+# Make a prediction based on the user-entered data
+# Returns the predicted price with its respective probability
 def predict_species(host_is_superhost,accommodates, bedrooms, beds, bathrooms, number_of_reviews):
     data_in = [[host_is_superhost,accommodates, bedrooms, beds, bathrooms, number_of_reviews]]
     prediction = model.predict(data_in)
